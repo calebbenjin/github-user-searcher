96e8fbf515eb11837f61c13e37678694295d4274
calebbenjin <calebben94@gmail.com>
2021-02-20 13:09:14 +0100

    error page setup

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+
+
+
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <h1>404</h1>
+      <h3>Sorry, the page your tried can not be found</h3>
+      <Link to="/" className="btn">
+        Back Home
+      </Link>
+    </Wrapper>
+  )
 };
+
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
